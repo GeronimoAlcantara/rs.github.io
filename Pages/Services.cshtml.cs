92259ff3541f5017e3a46b7d15c38459784a1d7d
GeronimoAlcantara <galcantarajr@yahoo.com>
2024-01-18 12:32:11 +0000

    changes 3

---
+++ Pages/Services.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Robusoft.Pages
+{
+    public class ServicesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
